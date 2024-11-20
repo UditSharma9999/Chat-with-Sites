db5be15d3f26959b292166697b3ef9153ff10c31
udit sharma <63443176+UditSharma9999@users.noreply.github.com>
2024-11-20 22:19:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ from langchain.chains import create_history_aware_retriever, create_retrieval_ch
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_huggingface import HuggingFaceEndpoint
 
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
 #dotenv.load_dotenv()
 os.getenv('API_TOKEN')
 
